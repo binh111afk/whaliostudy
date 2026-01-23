3906924ab6092b205075618bb6b68a31734e5b67
Quang Binh <quangbinh1412007@gmail.com>
2026-01-23 18:03:59 +0700

    ok

--- js/docs.js
+++ js/docs.js
@@ -911,10 +911,6 @@ export const DocumentManager = {
         const barFile = document.getElementById('storage-bar-file');
         const barImage = document.getElementById('storage-bar-image');
 
-        if (barImage) {
-            barImage.innerHTML = `<svg width="14" height="14" fill="currentColor" viewBox="0 0 24 24" style="display: inline-block; vertical-align: middle; margin-right: 4px;"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/></svg>`;
-        }
-
         if (barFile) barFile.style.width = filePercent + '%';
         if (barImage) barImage.style.width = imagePercent + '%';
 
