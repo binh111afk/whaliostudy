c76db7a80efd065bd1b1243bbebe74be75f8fe92
Quang Binh <quangbinh1412007@gmail.com>
2026-02-21 02:45:27 +0700

    thêm cors

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ const corsOptions = {
         'http://localhost:3000',      // Express server
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
+        'https://whaliostudying.onrender.com',  // Frontend trên Render
         /\.vercel\.app$/,             // Vercel deployments
         /\.netlify\.app$/             // Netlify deployments
     ],
